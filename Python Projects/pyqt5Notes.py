dc0caf10f9d1c080840d768773401bbdd0cb2d85
EnzoMGitHub <vincenzom9@gmail.com>
2023-06-19 19:19:15 -0400

    Added comments to my notes

--- Python Projects/pyqt5Notes.py
+++ Python Projects/pyqt5Notes.py
@@ -2,14 +2,14 @@ from PyQt5.QtWidgets import QApplication, QPushButton, QWidget
 
 # How to create a button
 # Button Class
-class Button(QWidget):
+class UiStuffs(QWidget):
     # constructor is below
     def __init__(self):
-        # calls parent constructor
+        # calls the constructor of the inherited Qwidget class
         super().__init__()
         # tells pyqt5 that im boutta do some ui stuff
         self.initUI()
-    # Creates the button
+    # Initializes the user interface
     def initUI(self):
         button = QPushButton('Click me', self) # makes a button object and stores it in the button variable, the string is the text that is shown on the button
         button.clicked.connect(self.buttonClicked) # When the button is clicked it calls the buttonClicked function
@@ -22,7 +22,8 @@ class Button(QWidget):
         # code here
         print('Button clicked')
 
+# The if statement checks if the script this is being run in is the main script
 if __name__ == '__main__':
-    app = QApplication([])
-    ex = Button()
-    app.exec_()
+    app = QApplication([]) # creates an instance of Qapplication which manages the GUI application
+    ex = UiStuffs() # This line creates an instance of the Example class, which represents the main window of the application.
+    app.exec_() # This line starts the main event loop of the application, which waits for events (such as button clicks) and handles them.
